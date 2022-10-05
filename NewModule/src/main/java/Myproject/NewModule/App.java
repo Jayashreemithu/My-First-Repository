5a272c05acb345b4fefd0fcd69bd34816b69551f
Jayashree <Jayashree@LAPTOP-0HKEBI9K>
2022-10-05 20:21:08 +0530

    my first commit

---
+++ NewModule/src/main/java/Myproject/NewModule/App.java
@@ -0,0 +1,14 @@
+package Myproject.NewModule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("goodmorning");
+    }
+}
