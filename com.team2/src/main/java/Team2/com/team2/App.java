5aa5f1c4e923f5be6b10fc84590cdd7b132869ba
Jayashree <Jayashree@LAPTOP-0HKEBI9K>
2022-10-05 18:40:01 +0530

    My first commit to first repository

---
+++ com.team2/src/main/java/Team2/com/team2/App.java
@@ -0,0 +1,13 @@
+package Team2.com.team2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.team2/src/main/java/Team2/com/team2/Loginpage.java
@@ -0,0 +1,8 @@
+package Team2.com.team2;
+
+public class Loginpage {
+	public static void main(String[] args) {
+		System.out.println("our new Project started");
+	}
+
+}
